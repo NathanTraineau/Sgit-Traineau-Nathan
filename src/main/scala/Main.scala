012af9c3976a6cb62e8a918412645f67a01345b0
White Jacques <32514062+NathanTraineau@users.noreply.github.com>
2019-10-20 16:50:50 +0200

    Adding the source code

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+import Create.Repository
+import IO.Command_Parser
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+      val rep = scala.io.StdIn.readLine()
+      Command_Parser.parser(rep);
+
+  }
+
+
+}
